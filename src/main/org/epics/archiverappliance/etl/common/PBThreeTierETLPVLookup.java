397339aa5ca1d28802fb4433c5fd79afe9551609
slac-mshankar <devnull@localhost>
2015-03-17 10:00:41 -0700

    Bug reported by Jud. The corner case for determing if we have enough ETL threads is incorrect. It should be > not >=

--- src/main/org/epics/archiverappliance/etl/common/PBThreeTierETLPVLookup.java
+++ src/main/org/epics/archiverappliance/etl/common/PBThreeTierETLPVLookup.java
@@ -148,7 +148,7 @@ public final class PBThreeTierETLPVLookup {
 				return;
 			}
 
-			if((dataSources.length-1) >= applianceMetrics.length) { 
+			if((dataSources.length-1) > applianceMetrics.length) { 
 				configlogger.fatal("The pv " + pvName + " has " + dataSources.length + " datasources. This installation is configured for a max of " + (applianceMetrics.length+1) + " datasources. To change this, please edit the archappl.properties for your installation and change the " + MAXIMUM_NUMBER_OF_LIFETIMES_IN_INSTALLATION + " to at least " + dataSources.length);
 				return;
 			}
