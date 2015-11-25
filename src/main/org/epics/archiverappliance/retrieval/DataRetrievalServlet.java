6e2620230d6d675fcdb0549ca49288dc6d66a9ff
Michael Kenning <michael.kenning@diamond.ac.uk>
2015-11-25 11:36:36 +0000

    Missing class necessary for cluster retrieval has been added

--- src/main/org/epics/archiverappliance/retrieval/DataRetrievalServlet.java
+++ src/main/org/epics/archiverappliance/retrieval/DataRetrievalServlet.java
@@ -1574,4 +1574,58 @@ public class DataRetrievalServlet  extends HttpServlet {
 		
 		return false;
 	}
+	
+    /** 
+     * <p> 
+     * This class should be used to store the PV name and type info of data that will be 
+     * retrieved from neighbouring nodes in a cluster, to be returned in a response from 
+     * the source cluster. 
+     * </p> 
+     * <p> 
+     * PVTypeInfo maintains a PV name field, too. At first the PV name field in this object 
+     * seems superfluous. But the field is necessary, as it contains the unprocessed PV 
+     * name as opposed to the PV name stored by the PVTypeInfo object, which has been 
+     * processed. 
+     * </p> 
+     *  
+     * @author Michael Kenning 
+     * 
+     */ 
+    private class PVInfoForClusterRetrieval { 
+             
+            private String pvName; 
+            private PVTypeInfo typeInfo; 
+            private PostProcessor postProcessor; 
+            private ApplianceInfo applianceInfo; 
+             
+            private PVInfoForClusterRetrieval(String pvName, PVTypeInfo typeInfo,  
+                            PostProcessor postProcessor, ApplianceInfo applianceInfo) { 
+                    this.pvName = pvName; 
+                    this.typeInfo = typeInfo; 
+                    this.postProcessor = postProcessor; 
+                    this.applianceInfo = applianceInfo; 
+                     
+                    assert(this.pvName == null); 
+                    assert(this.typeInfo == null); 
+                    assert(this.postProcessor == null); 
+                    assert(this.applianceInfo == null); 
+            } 
+             
+            public String getPVName() { 
+                    return pvName; 
+            } 
+             
+            public PVTypeInfo getTypeInfo() { 
+                    return typeInfo; 
+            } 
+             
+            public PostProcessor getPostProcessor() { 
+                    return postProcessor; 
+            } 
+             
+            public ApplianceInfo getApplianceInfo() { 
+                    return applianceInfo; 
+            } 
+
+    }
 }
