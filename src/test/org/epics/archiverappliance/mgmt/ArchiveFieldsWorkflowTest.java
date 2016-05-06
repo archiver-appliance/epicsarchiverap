c5c02e4718a7819b1a86801cc6e50b3ebaeb4e92
Murali Shankar <mshankar@slac.stanford.edu>
2016-05-06 13:28:52 -0700

    CLose the browser before closing tomcat

--- src/test/org/epics/archiverappliance/mgmt/ArchiveFieldsWorkflowTest.java
+++ src/test/org/epics/archiverappliance/mgmt/ArchiveFieldsWorkflowTest.java
@@ -33,8 +33,8 @@ public class ArchiveFieldsWorkflowTest {
 
 	@After
 	public void tearDown() throws Exception {
-		tomcatSetup.tearDown();
 		driver.quit();
+		tomcatSetup.tearDown();
 		siocSetup.stopSIOC();
 	}
 
