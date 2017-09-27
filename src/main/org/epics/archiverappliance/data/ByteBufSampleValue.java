9ecdeb590bccb5a706f70311b59f267df70def1a
Murali Shankar <mshankar@slac.stanford.edu>
2017-09-27 16:24:01 -0700

    Add initial code to convert pvData to JSON

--- src/main/org/epics/archiverappliance/data/ByteBufSampleValue.java
+++ src/main/org/epics/archiverappliance/data/ByteBufSampleValue.java
@@ -2,8 +2,17 @@ package org.epics.archiverappliance.data;
 
 import java.nio.ByteBuffer;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
-import org.epics.archiverappliance.data.SampleValue;
+import org.epics.pvaccess.impl.remote.IntrospectionRegistry;
+import org.epics.pvaccess.impl.remote.SerializationHelper;
+import org.epics.pvdata.pv.DeserializableControl;
+import org.epics.pvdata.pv.Field;
+import org.epics.pvdata.pv.PVField;
+import org.epics.pvdata.pv.PVStructure;
+import org.epics.pvdata.pv.Type;
+import org.json.simple.JSONValue;
 
 /**
  * A sample value that encapsulates a byte buffer.
@@ -32,10 +41,42 @@ public class ByteBufSampleValue implements SampleValue {
 	public int hashCode() {
 		return this.buf.hashCode();
 	}
+	
+	private void pvStructure2JSON(PVStructure pvStructure, Map<String, Object> ret) {
+		for(PVField fld : pvStructure.getPVFields()) {
+			String fieldName = fld.getFieldName();
+			Type type = fld.getField().getType();
+			switch(type) {
+			case scalar: { 
+				ret.put(fieldName, fld.toString());
+				continue;
+			}
+			case structure: {
+				PVStructure childStruct = pvStructure.getStructureField(fieldName);
+				assert(childStruct != null);
+				Map<String, Object> childMap = new TreeMap<String, Object>();
+				pvStructure2JSON(childStruct, childMap);
+				ret.put(fieldName, childMap);
+				continue;
+			}
+			case scalarArray:
+			case structureArray:
+			case union:
+			case unionArray:
+			default: {
+				throw new UnsupportedOperationException();
+			}
+			}
+			
+		}
+	}
 
 	@Override
 	public String toJSONString() {
-		throw new UnsupportedOperationException();
+		Map<String, Object> ret = new TreeMap<String, Object>();
+		PVStructure pvStructure = SerializationHelper.deserializeStructureFull(this.buf, new DummyDeserializationControl());
+		pvStructure2JSON(pvStructure, ret);
+		return JSONValue.toJSONString(ret);
 	}
 
 	@SuppressWarnings("rawtypes")
@@ -62,5 +103,22 @@ public class ByteBufSampleValue implements SampleValue {
 	@Override
 	public int getElementCount() {
 		return 1;
+	}
+
+	class DummyDeserializationControl implements DeserializableControl {
+		protected final IntrospectionRegistry incomingIR = new IntrospectionRegistry();
+
+		@Override
+		public void alignData(int arg0) {
+		}
+
+		@Override
+		public Field cachedDeserialize(ByteBuffer buffer) {
+			return incomingIR.deserialize(buffer, this);
+		}
+
+		@Override
+		public void ensureData(int arg0) {
+		}        
 	}
 }
