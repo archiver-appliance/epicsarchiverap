93d0a1afc4128c7a3f5f488e964f86038876528b
Kunal Shroff <shroffk@bnl.gov>
2018-01-18 16:08:13 -0500

    Adding utility class for easing use of NTTypes

---
+++ src/main/org/epics/archiverappliance/mgmt/pva/actions/NTUtil.java
@@ -0,0 +1,26 @@
+package org.epics.archiverappliance.mgmt.pva.actions;
+
+import org.epics.pvdata.pv.PVStringArray;
+import org.epics.pvdata.pv.StringArrayData;
+
+/**
+ * A helper class to easily extract value from nt structures
+ * 
+ * @author Kunal Shroff
+ *
+ */
+public class NTUtil {
+
+	/**
+	 * extract the string array values from a pvStringArray
+	 * @param pvStringArray
+	 * @return
+	 */
+	@SuppressWarnings("unused")
+	public static String[] extractStringArray(PVStringArray pvStringArray) {
+		StringArrayData data = new StringArrayData();
+		int len = pvStringArray.get(0, pvStringArray.getLength(), data);
+		return data.data;
+	}
+
+}
