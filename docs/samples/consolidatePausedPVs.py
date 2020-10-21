81e7a3867472170b0fa04ca1cc41f0788a601c5b
Murali Shankar <mshankar@slac.stanford.edu>
2020-10-21 14:42:00 -0700

    Add a script to consolidate all paused PVs.
    The MTS can get cluttered up with data from PV's that have been paused 
    for a while and might never come back. Use this script to consolidate 
    the data for the PV's all the way to the LTS.

---
+++ docs/samples/consolidatePausedPVs.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+'''Find the list of paused PV's in the archiver and consolidate the data for these PV's all the way to the specified store'''
+
+import os
+import sys
+import argparse
+import json
+import requests
+
+def getPausedPVs(bplURL):
+    '''Get the list of paused PV's'''
+    resp = requests.get(bplURL + '/getPausedPVsReport')
+    resp.raise_for_status()
+    return [x["pvName"] for x in resp.json()]
+
+def consolidateDataForPV(bplURL, storename, pvname):
+    '''Consolidate the data for the specificed PV to the specified store'''
+    resp = requests.get(bplURL + '/consolidateDataForPV', params={"pv": pvname, "storage": storename})
+    resp.raise_for_status()
+    return True
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("url", help="This is the URL to the mgmt bpl interface of the appliance cluster. For example, http://arch.slac.stanford.edu/mgmt/bpl")
+    parser.add_argument("store", help="The name of the store to consolidate the data")
+    args = parser.parse_args()
+    pvnames = getPausedPVs(args.url)
+    for pvname in pvnames:
+        print("Consolidating data for ", pvname)
+        consolidateDataForPV(args.url, args.store, pvname)
