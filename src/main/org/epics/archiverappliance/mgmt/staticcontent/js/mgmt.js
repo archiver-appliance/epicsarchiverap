cee31934ba046f7eb17d55ade7e9b2895c22e692
loic.caouen <loic.caouen@cea.fr>
2023-03-15 10:32:24 +0100

    Add a Delete button in the GUI #158

--- src/main/org/epics/archiverappliance/mgmt/staticcontent/js/mgmt.js
+++ src/main/org/epics/archiverappliance/mgmt/staticcontent/js/mgmt.js
@@ -1275,6 +1275,44 @@ function pauseMultiplePVs() {
 	});
 }
 
+function deleteMultiplePVs() {
+	
+	var pvQuery = getPVQueryParam();
+	if(!pvQuery) return;
+	
+	var jsonurl = '../bpl/deletePV?' + pvQuery;
+	var components = jsonurl.split('?');
+	var urlalone = components[0];
+	var querystring = '';
+	if(components.length > 1) {
+		querystring = components[1];
+	}
+	var HTTPMethod = 'GET';
+	if(jsonurl.length > 2048) {
+		HTTPMethod = 'POST';
+	}
+	
+	$.ajax({
+		url: urlalone,
+		data: querystring,
+		type: HTTPMethod,
+		dataType: 'json',
+		success: function(data, textStatus, jqXHR) {
+			if(data.status != null && data.status != undefined && data.status == "ok") {
+				getPVNames();
+			} else if(data.validation != null && data.validation != undefined){
+				alert(data.validation);
+			} else {
+				alert("deleteMultiplePVs returned something valid but did not have a status field.");
+			}
+
+		},
+		error: function(jqXHR, textStatus, errorThrown) {
+			alert("An error occured on the server while deleting PVs -- " + textStatus + " -- " + errorThrown);
+		}
+	});
+}
+
 function resumeMultiplePVs() {
 	
 	var pvQuery = getPVQueryParam();
