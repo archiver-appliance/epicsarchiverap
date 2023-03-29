9f43eb4f6efcc8644f08378bd0875355770f2fa4
Murali Shankar <mshankar@slac.stanford.edu>
2023-03-28 17:37:27 -0700

    More places where we do not let the BPL succeed until the cluster has started up

--- src/main/org/epics/archiverappliance/mgmt/bpl/UploadChannelArchiverConfigAction.java
+++ src/main/org/epics/archiverappliance/mgmt/bpl/UploadChannelArchiverConfigAction.java
@@ -42,7 +42,12 @@ public class UploadChannelArchiverConfigAction implements BPLAction {
 	@Override
 	public void execute(HttpServletRequest req, HttpServletResponse resp,
 			ConfigService configService) throws IOException {
-		
+		if(!configService.hasClusterFinishedInitialization()) {
+			// If you have defined spare appliances in the appliances.xml that will never come up; you should remove them
+			// This seems to be one of the few ways we can prevent split brain clusters from messing up the pv <-> appliance mapping.
+			throw new IOException("Waiting for all the appliances listed in appliances.xml to finish loading up their PVs into the cluster");
+		}
+				
 		// Check that we have a file upload request
 		boolean isMultipart = ServletFileUpload.isMultipartContent(req);
 		if(!isMultipart) {
