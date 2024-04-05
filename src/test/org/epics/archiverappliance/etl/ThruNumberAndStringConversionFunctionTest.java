60bb5cbd57f978c462c77965cd79d8c7605252bf
Sky Brewer <sky.brewer@ess.eu>
2024-04-05 11:31:45 +0200

    Reformat files
    Format files in test.org.epics.archiverappliance.etl

--- src/test/org/epics/archiverappliance/etl/ThruNumberAndStringConversionFunctionTest.java
+++ src/test/org/epics/archiverappliance/etl/ThruNumberAndStringConversionFunctionTest.java
@@ -32,130 +32,148 @@ import java.util.Set;
 import static org.junit.Assert.assertTrue;
 
 /**
- * Test various type conversion functions. 
+ * Test various type conversion functions.
  * Some type conversions are trivial; others are more complex.
- * We generate some sample events of a particular type; then convert to other dest types and convert back. 
+ * We generate some sample events of a particular type; then convert to other dest types and convert back.
  * @author mshankar
  *
  */
 public class ThruNumberAndStringConversionFunctionTest {
-	private static Logger logger = LogManager.getLogger(ThruNumberAndStringConversionFunctionTest.class.getName());
+    private static Logger logger = LogManager.getLogger(ThruNumberAndStringConversionFunctionTest.class.getName());
 
+    @Test
+    public void testThruNumberConversion() throws Exception {
+        testThruNumberConversionForDBRType(ArchDBRTypes.DBR_SCALAR_INT, ArchDBRTypes.DBR_SCALAR_DOUBLE);
+        testThruNumberConversionForDBRType(ArchDBRTypes.DBR_SCALAR_ENUM, ArchDBRTypes.DBR_SCALAR_DOUBLE);
+        testThruNumberConversionForDBRType(ArchDBRTypes.DBR_SCALAR_FLOAT, ArchDBRTypes.DBR_SCALAR_DOUBLE);
+        testThruNumberConversionForDBRType(ArchDBRTypes.DBR_SCALAR_ENUM, ArchDBRTypes.DBR_SCALAR_INT);
+        testThruNumberConversionForDBRType(ArchDBRTypes.DBR_SCALAR_INT, ArchDBRTypes.DBR_SCALAR_ENUM);
+        testThruNumberConversionForDBRType(ArchDBRTypes.DBR_SCALAR_DOUBLE, ArchDBRTypes.DBR_SCALAR_ENUM);
+        testThruNumberConversionForDBRType(ArchDBRTypes.DBR_SCALAR_DOUBLE, ArchDBRTypes.DBR_SCALAR_INT);
+        testThruNumberConversionForDBRType(ArchDBRTypes.DBR_SCALAR_DOUBLE, ArchDBRTypes.DBR_SCALAR_FLOAT);
+    }
 
-	@Test
-	public void testThruNumberConversion() throws Exception {
-		testThruNumberConversionForDBRType(ArchDBRTypes.DBR_SCALAR_INT, ArchDBRTypes.DBR_SCALAR_DOUBLE);
-		testThruNumberConversionForDBRType(ArchDBRTypes.DBR_SCALAR_ENUM, ArchDBRTypes.DBR_SCALAR_DOUBLE);
-		testThruNumberConversionForDBRType(ArchDBRTypes.DBR_SCALAR_FLOAT, ArchDBRTypes.DBR_SCALAR_DOUBLE);
-		testThruNumberConversionForDBRType(ArchDBRTypes.DBR_SCALAR_ENUM, ArchDBRTypes.DBR_SCALAR_INT);
-		testThruNumberConversionForDBRType(ArchDBRTypes.DBR_SCALAR_INT, ArchDBRTypes.DBR_SCALAR_ENUM);
-		testThruNumberConversionForDBRType(ArchDBRTypes.DBR_SCALAR_DOUBLE, ArchDBRTypes.DBR_SCALAR_ENUM);
-		testThruNumberConversionForDBRType(ArchDBRTypes.DBR_SCALAR_DOUBLE, ArchDBRTypes.DBR_SCALAR_INT);
-		testThruNumberConversionForDBRType(ArchDBRTypes.DBR_SCALAR_DOUBLE, ArchDBRTypes.DBR_SCALAR_FLOAT);
-	}
-	
-	private void testThruNumberConversionForDBRType(ArchDBRTypes srcDBRType, ArchDBRTypes destDBRType) throws Exception {
-		logger.info("Testing conversion from " + srcDBRType.toString() + " to " + destDBRType.toString());
-		EventStream srcStream = generateDataForArchDBRType(srcDBRType);
-		EventStream destStream = convertToType(srcStream, destDBRType);
-		compareStreams(srcStream, destStream);
-	}
+    private void testThruNumberConversionForDBRType(ArchDBRTypes srcDBRType, ArchDBRTypes destDBRType)
+            throws Exception {
+        logger.info("Testing conversion from " + srcDBRType.toString() + " to " + destDBRType.toString());
+        EventStream srcStream = generateDataForArchDBRType(srcDBRType);
+        EventStream destStream = convertToType(srcStream, destDBRType);
+        compareStreams(srcStream, destStream);
+    }
 
-	private EventStream generateDataForArchDBRType(ArchDBRTypes dbrType) throws Exception { 
-		int numEvents = 500;
+    private EventStream generateDataForArchDBRType(ArchDBRTypes dbrType) throws Exception {
+        int numEvents = 500;
         short currentYear = TimeUtils.getCurrentYear();
-        ArrayListEventStream ret = new ArrayListEventStream(numEvents, new RemotableEventStreamDesc(dbrType, "test", currentYear));
-		int eventsAdded = 0;
-		Constructor<? extends DBRTimeEvent> serializingConstructor = DBR2PBTypeMapping.getPBClassFor(dbrType).getSerializingConstructor();
+        ArrayListEventStream ret =
+                new ArrayListEventStream(numEvents, new RemotableEventStreamDesc(dbrType, "test", currentYear));
+        int eventsAdded = 0;
+        Constructor<? extends DBRTimeEvent> serializingConstructor =
+                DBR2PBTypeMapping.getPBClassFor(dbrType).getSerializingConstructor();
 
-        try (SimulationEventStream simstream = new SimulationEventStream(dbrType, new ValueGenerator(dbrType), TimeUtils.getStartOfYear(currentYear), TimeUtils.getEndOfYear(currentYear), 1)) {
-			for(Event simEvent : simstream) {
-				DBRTimeEvent genEvent = (DBRTimeEvent) serializingConstructor.newInstance(simEvent);
-				if(eventsAdded % 1000 == 0) { 
-					genEvent.addFieldValue("HIHI", "Test");
-					genEvent.addFieldValue("LOLO", "13:40:12");
-					if(eventsAdded % 2000 == 0) {
-						genEvent.markAsActualChange();
-					}
-				}
+        try (SimulationEventStream simstream = new SimulationEventStream(
+                dbrType,
+                new ValueGenerator(dbrType),
+                TimeUtils.getStartOfYear(currentYear),
+                TimeUtils.getEndOfYear(currentYear),
+                1)) {
+            for (Event simEvent : simstream) {
+                DBRTimeEvent genEvent = (DBRTimeEvent) serializingConstructor.newInstance(simEvent);
+                if (eventsAdded % 1000 == 0) {
+                    genEvent.addFieldValue("HIHI", "Test");
+                    genEvent.addFieldValue("LOLO", "13:40:12");
+                    if (eventsAdded % 2000 == 0) {
+                        genEvent.markAsActualChange();
+                    }
+                }
 
-				ret.add(genEvent);
-				if(eventsAdded++ > numEvents) break;
-			}
-			return ret;
-		}
-	}
-	
-	private EventStream convertToType(EventStream srcStream, ArchDBRTypes destDBRType) throws IOException { 
-		return new ThruNumberAndStringConversion(destDBRType).convertStream(srcStream, null, null);
-	}
-	
-	private void compareStreams(EventStream srcStream, EventStream destStream) throws IOException {
-		Iterator<Event> srcIt = srcStream.iterator();
-		Iterator<Event> destIt = destStream.iterator();
-		int eventCount = 0;
-		while(srcIt.hasNext() || destIt.hasNext()) { 
-			DBRTimeEvent srcEvent = (DBRTimeEvent) srcIt.next();
-			DBRTimeEvent destEvent = (DBRTimeEvent) destIt.next();
-			assertTrue("Compare timestamps failed at event " + eventCount, srcEvent.getEventTimeStamp().equals(destEvent.getEventTimeStamp()));
-			assertTrue("Compare status failed at event " + eventCount, srcEvent.getStatus() == destEvent.getStatus());
-			assertTrue("Compare severity failed at event " + eventCount, srcEvent.getSeverity() == destEvent.getSeverity());
-			assertTrue("Compare value failed at event " + eventCount 
-					+ " " + srcEvent.getSampleValue().getValue().doubleValue()
-					+ " " + destEvent.getSampleValue().getValue().doubleValue()
-					+ " " + Math.abs(srcEvent.getSampleValue().getValue().doubleValue() - destEvent.getSampleValue().getValue().doubleValue()), 
-					Math.abs(srcEvent.getSampleValue().getValue().doubleValue() - destEvent.getSampleValue().getValue().doubleValue()) < 0.0005);
-			assertTrue("Compare fields failed at event " + eventCount, compareMaps(srcEvent.getFields(), destEvent.getFields()));
-			assertTrue("Compare fields changed failed at event " + eventCount, srcEvent.isActualChange() == destEvent.isActualChange());
-			eventCount++;
-		}
-	}
-	
-	private static boolean compareMaps(HashMap<String, String> map1, HashMap<String, String> map2) { 
-		Set<String> removedKeys = new HashSet<String>(map1.keySet());
-		removedKeys.removeAll(map2.keySet());
+                ret.add(genEvent);
+                if (eventsAdded++ > numEvents) break;
+            }
+            return ret;
+        }
+    }
 
-		Set<String> addedKeys = new HashSet<String>(map2.keySet());
-		addedKeys.removeAll(map1.keySet());
+    private EventStream convertToType(EventStream srcStream, ArchDBRTypes destDBRType) throws IOException {
+        return new ThruNumberAndStringConversion(destDBRType).convertStream(srcStream, null, null);
+    }
 
-		Set<Entry<String, String>> changedEntries = new HashSet<Entry<String, String>>(map2.entrySet());
-		changedEntries.removeAll(map1.entrySet());
-		
-		return removedKeys.isEmpty() && addedKeys.isEmpty() && changedEntries.isEmpty();
-	}
+    private void compareStreams(EventStream srcStream, EventStream destStream) throws IOException {
+        Iterator<Event> srcIt = srcStream.iterator();
+        Iterator<Event> destIt = destStream.iterator();
+        int eventCount = 0;
+        while (srcIt.hasNext() || destIt.hasNext()) {
+            DBRTimeEvent srcEvent = (DBRTimeEvent) srcIt.next();
+            DBRTimeEvent destEvent = (DBRTimeEvent) destIt.next();
+            assertTrue(
+                    "Compare timestamps failed at event " + eventCount,
+                    srcEvent.getEventTimeStamp().equals(destEvent.getEventTimeStamp()));
+            assertTrue("Compare status failed at event " + eventCount, srcEvent.getStatus() == destEvent.getStatus());
+            assertTrue(
+                    "Compare severity failed at event " + eventCount,
+                    srcEvent.getSeverity() == destEvent.getSeverity());
+            assertTrue(
+                    "Compare value failed at event " + eventCount
+                            + " " + srcEvent.getSampleValue().getValue().doubleValue()
+                            + " " + destEvent.getSampleValue().getValue().doubleValue()
+                            + " "
+                            + Math.abs(srcEvent.getSampleValue().getValue().doubleValue()
+                                    - destEvent.getSampleValue().getValue().doubleValue()),
+                    Math.abs(srcEvent.getSampleValue().getValue().doubleValue()
+                                    - destEvent.getSampleValue().getValue().doubleValue())
+                            < 0.0005);
+            assertTrue(
+                    "Compare fields failed at event " + eventCount,
+                    compareMaps(srcEvent.getFields(), destEvent.getFields()));
+            assertTrue(
+                    "Compare fields changed failed at event " + eventCount,
+                    srcEvent.isActualChange() == destEvent.isActualChange());
+            eventCount++;
+        }
+    }
 
+    private static boolean compareMaps(HashMap<String, String> map1, HashMap<String, String> map2) {
+        Set<String> removedKeys = new HashSet<String>(map1.keySet());
+        removedKeys.removeAll(map2.keySet());
+
+        Set<String> addedKeys = new HashSet<String>(map2.keySet());
+        addedKeys.removeAll(map1.keySet());
+
+        Set<Entry<String, String>> changedEntries = new HashSet<Entry<String, String>>(map2.entrySet());
+        changedEntries.removeAll(map1.entrySet());
+
+        return removedKeys.isEmpty() && addedKeys.isEmpty() && changedEntries.isEmpty();
+    }
 
     private static class ValueGenerator implements SimulationValueGenerator {
         private final ArchDBRTypes dbrType;
 
         public ValueGenerator(ArchDBRTypes dbrType) {
-			this.dbrType = dbrType;
-		}
+            this.dbrType = dbrType;
+        }
 
-		@Override
-		public SampleValue getSampleValue(ArchDBRTypes type, int secondsIntoYear) {
+        @Override
+        public SampleValue getSampleValue(ArchDBRTypes type, int secondsIntoYear) {
             return switch (dbrType) {
                 case DBR_SCALAR_BYTE -> new ScalarValue<Byte>((byte) secondsIntoYear);
                 case DBR_SCALAR_DOUBLE -> new ScalarValue<Double>((double) secondsIntoYear);
                 case DBR_SCALAR_ENUM, DBR_SCALAR_SHORT -> new ScalarValue<Short>((short) secondsIntoYear);
                 case DBR_SCALAR_FLOAT -> new ScalarValue<Float>((float) secondsIntoYear);
                 case DBR_SCALAR_INT -> new ScalarValue<Integer>((int) secondsIntoYear);
-                case DBR_SCALAR_STRING, DBR_V4_GENERIC_BYTES ->
-                        new ScalarStringSampleValue(Integer.toString(secondsIntoYear));
-                case DBR_WAVEFORM_BYTE ->
-                        new VectorValue<Byte>(Collections.nCopies(10 * secondsIntoYear, ((byte) (secondsIntoYear % 255))));
-                case DBR_WAVEFORM_DOUBLE ->
-                        new VectorValue<Double>(Collections.nCopies(10 * secondsIntoYear, ((double) secondsIntoYear)));
-                case DBR_WAVEFORM_ENUM, DBR_WAVEFORM_SHORT ->
-                        new VectorValue<Short>(Collections.nCopies(10 * secondsIntoYear, ((short) secondsIntoYear)));
-                case DBR_WAVEFORM_FLOAT ->
-                        new VectorValue<Float>(Collections.nCopies(10 * secondsIntoYear, ((float) secondsIntoYear)));
-                case DBR_WAVEFORM_INT ->
-                        new VectorValue<Integer>(Collections.nCopies(10 * secondsIntoYear, ((int) secondsIntoYear)));
-                case DBR_WAVEFORM_STRING ->
-                        new VectorStringSampleValue(Collections.nCopies(10 * secondsIntoYear, Integer.toString(secondsIntoYear)));
+                case DBR_SCALAR_STRING, DBR_V4_GENERIC_BYTES -> new ScalarStringSampleValue(
+                        Integer.toString(secondsIntoYear));
+                case DBR_WAVEFORM_BYTE -> new VectorValue<Byte>(
+                        Collections.nCopies(10 * secondsIntoYear, ((byte) (secondsIntoYear % 255))));
+                case DBR_WAVEFORM_DOUBLE -> new VectorValue<Double>(
+                        Collections.nCopies(10 * secondsIntoYear, ((double) secondsIntoYear)));
+                case DBR_WAVEFORM_ENUM, DBR_WAVEFORM_SHORT -> new VectorValue<Short>(
+                        Collections.nCopies(10 * secondsIntoYear, ((short) secondsIntoYear)));
+                case DBR_WAVEFORM_FLOAT -> new VectorValue<Float>(
+                        Collections.nCopies(10 * secondsIntoYear, ((float) secondsIntoYear)));
+                case DBR_WAVEFORM_INT -> new VectorValue<Integer>(
+                        Collections.nCopies(10 * secondsIntoYear, ((int) secondsIntoYear)));
+                case DBR_WAVEFORM_STRING -> new VectorStringSampleValue(
+                        Collections.nCopies(10 * secondsIntoYear, Integer.toString(secondsIntoYear)));
             };
-		} 
-		
-	}
+        }
+    }
 }
