030680f69db026de60a7728c9668f9dffb885d99
Sky Brewer <sky.brewer@ess.eu>
2024-04-05 11:28:31 +0200

    Reformat files
    Format files in test.org.epics.archiverappliance.config

--- src/test/org/epics/archiverappliance/config/ArchDBRTypesTest.java
+++ src/test/org/epics/archiverappliance/config/ArchDBRTypesTest.java
@@ -7,7 +7,6 @@
  *******************************************************************************/
 package org.epics.archiverappliance.config;
 
-import static org.junit.Assert.assertSame;
 import gov.aps.jca.dbr.DBR_TIME_Byte;
 import gov.aps.jca.dbr.DBR_TIME_Double;
 import gov.aps.jca.dbr.DBR_TIME_Enum;
@@ -15,10 +14,9 @@ import gov.aps.jca.dbr.DBR_TIME_Float;
 import gov.aps.jca.dbr.DBR_TIME_Int;
 import gov.aps.jca.dbr.DBR_TIME_Short;
 import gov.aps.jca.dbr.DBR_TIME_String;
-
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
+
+import static org.junit.Assert.assertSame;
 
 /**
  * Test the mapping for ArchDBRTypes
@@ -27,29 +25,70 @@ import org.junit.Test;
  */
 public class ArchDBRTypesTest {
 
+    @Test
+    public void testValueOf() {
+        JCA2ArchDBRType.values(); // Initialize the enum?
+        assertSame(
+                ArchDBRTypes.DBR_SCALAR_STRING.toString(),
+                ArchDBRTypes.DBR_SCALAR_STRING,
+                JCA2ArchDBRType.valueOf(new DBR_TIME_String()));
+        assertSame(
+                ArchDBRTypes.DBR_SCALAR_SHORT.toString(),
+                ArchDBRTypes.DBR_SCALAR_SHORT,
+                JCA2ArchDBRType.valueOf(new DBR_TIME_Short()));
+        assertSame(
+                ArchDBRTypes.DBR_SCALAR_FLOAT.toString(),
+                ArchDBRTypes.DBR_SCALAR_FLOAT,
+                JCA2ArchDBRType.valueOf(new DBR_TIME_Float()));
+        assertSame(
+                ArchDBRTypes.DBR_SCALAR_ENUM.toString(),
+                ArchDBRTypes.DBR_SCALAR_ENUM,
+                JCA2ArchDBRType.valueOf(new DBR_TIME_Enum()));
+        assertSame(
+                ArchDBRTypes.DBR_SCALAR_BYTE.toString(),
+                ArchDBRTypes.DBR_SCALAR_BYTE,
+                JCA2ArchDBRType.valueOf(new DBR_TIME_Byte()));
+        assertSame(
+                ArchDBRTypes.DBR_SCALAR_INT.toString(),
+                ArchDBRTypes.DBR_SCALAR_INT,
+                JCA2ArchDBRType.valueOf(new DBR_TIME_Int()));
+        assertSame(
+                ArchDBRTypes.DBR_SCALAR_DOUBLE.toString(),
+                ArchDBRTypes.DBR_SCALAR_DOUBLE,
+                JCA2ArchDBRType.valueOf(new DBR_TIME_Double()));
 
-	@Test
-	public void testValueOf() {
-		JCA2ArchDBRType.values(); // Initialize the enum?
-		assertSame(ArchDBRTypes.DBR_SCALAR_STRING.toString(), ArchDBRTypes.DBR_SCALAR_STRING, JCA2ArchDBRType.valueOf(new DBR_TIME_String()));
-		assertSame(ArchDBRTypes.DBR_SCALAR_SHORT.toString(), ArchDBRTypes.DBR_SCALAR_SHORT, JCA2ArchDBRType.valueOf(new DBR_TIME_Short()));
-		assertSame(ArchDBRTypes.DBR_SCALAR_FLOAT.toString(), ArchDBRTypes.DBR_SCALAR_FLOAT, JCA2ArchDBRType.valueOf(new DBR_TIME_Float()));
-		assertSame(ArchDBRTypes.DBR_SCALAR_ENUM.toString(), ArchDBRTypes.DBR_SCALAR_ENUM, JCA2ArchDBRType.valueOf(new DBR_TIME_Enum()));
-		assertSame(ArchDBRTypes.DBR_SCALAR_BYTE.toString(), ArchDBRTypes.DBR_SCALAR_BYTE, JCA2ArchDBRType.valueOf(new DBR_TIME_Byte()));
-		assertSame(ArchDBRTypes.DBR_SCALAR_INT.toString(), ArchDBRTypes.DBR_SCALAR_INT, JCA2ArchDBRType.valueOf(new DBR_TIME_Int()));
-		assertSame(ArchDBRTypes.DBR_SCALAR_DOUBLE.toString(), ArchDBRTypes.DBR_SCALAR_DOUBLE, JCA2ArchDBRType.valueOf(new DBR_TIME_Double()));
+        assertSame(
+                ArchDBRTypes.DBR_WAVEFORM_STRING.toString(),
+                ArchDBRTypes.DBR_WAVEFORM_STRING,
+                JCA2ArchDBRType.valueOf(new DBR_TIME_String(2)));
+        assertSame(
+                ArchDBRTypes.DBR_WAVEFORM_SHORT.toString(),
+                ArchDBRTypes.DBR_WAVEFORM_SHORT,
+                JCA2ArchDBRType.valueOf(new DBR_TIME_Short(2)));
+        assertSame(
+                ArchDBRTypes.DBR_WAVEFORM_FLOAT.toString(),
+                ArchDBRTypes.DBR_WAVEFORM_FLOAT,
+                JCA2ArchDBRType.valueOf(new DBR_TIME_Float(2)));
+        assertSame(
+                ArchDBRTypes.DBR_WAVEFORM_ENUM.toString(),
+                ArchDBRTypes.DBR_WAVEFORM_ENUM,
+                JCA2ArchDBRType.valueOf(new DBR_TIME_Enum(2)));
+        assertSame(
+                ArchDBRTypes.DBR_WAVEFORM_BYTE.toString(),
+                ArchDBRTypes.DBR_WAVEFORM_BYTE,
+                JCA2ArchDBRType.valueOf(new DBR_TIME_Byte(2)));
+        assertSame(
+                ArchDBRTypes.DBR_WAVEFORM_INT.toString(),
+                ArchDBRTypes.DBR_WAVEFORM_INT,
+                JCA2ArchDBRType.valueOf(new DBR_TIME_Int(2)));
+        assertSame(
+                ArchDBRTypes.DBR_WAVEFORM_DOUBLE.toString(),
+                ArchDBRTypes.DBR_WAVEFORM_DOUBLE,
+                JCA2ArchDBRType.valueOf(new DBR_TIME_Double(2)));
 
-		assertSame(ArchDBRTypes.DBR_WAVEFORM_STRING.toString(), ArchDBRTypes.DBR_WAVEFORM_STRING, JCA2ArchDBRType.valueOf(new DBR_TIME_String(2)));
-		assertSame(ArchDBRTypes.DBR_WAVEFORM_SHORT.toString(), ArchDBRTypes.DBR_WAVEFORM_SHORT, JCA2ArchDBRType.valueOf(new DBR_TIME_Short(2)));
-		assertSame(ArchDBRTypes.DBR_WAVEFORM_FLOAT.toString(), ArchDBRTypes.DBR_WAVEFORM_FLOAT, JCA2ArchDBRType.valueOf(new DBR_TIME_Float(2)));
-		assertSame(ArchDBRTypes.DBR_WAVEFORM_ENUM.toString(), ArchDBRTypes.DBR_WAVEFORM_ENUM, JCA2ArchDBRType.valueOf(new DBR_TIME_Enum(2)));
-		assertSame(ArchDBRTypes.DBR_WAVEFORM_BYTE.toString(), ArchDBRTypes.DBR_WAVEFORM_BYTE, JCA2ArchDBRType.valueOf(new DBR_TIME_Byte(2)));
-		assertSame(ArchDBRTypes.DBR_WAVEFORM_INT.toString(), ArchDBRTypes.DBR_WAVEFORM_INT, JCA2ArchDBRType.valueOf(new DBR_TIME_Int(2)));
-		assertSame(ArchDBRTypes.DBR_WAVEFORM_DOUBLE.toString(), ArchDBRTypes.DBR_WAVEFORM_DOUBLE, JCA2ArchDBRType.valueOf(new DBR_TIME_Double(2)));
-		
-		// TODO - Fix once we have upgraded the EPICS V4 jars.
-		// assertSame(ArchDBRTypes.DBR_V4_GENERIC_BYTES.toString(), ArchDBRTypes.DBR_V4_GENERIC_BYTES, EPICSV42DBRType.valueOf(DataType_EPICSV4.TIME_VSTATIC_BYTES));
+        // TODO - Fix once we have upgraded the EPICS V4 jars.
+        // assertSame(ArchDBRTypes.DBR_V4_GENERIC_BYTES.toString(), ArchDBRTypes.DBR_V4_GENERIC_BYTES,
+        // EPICSV42DBRType.valueOf(DataType_EPICSV4.TIME_VSTATIC_BYTES));
 
-	}
-
+    }
 }
--- src/test/org/epics/archiverappliance/config/PVNameRegexTest.java
+++ src/test/org/epics/archiverappliance/config/PVNameRegexTest.java
@@ -1,8 +1,8 @@
 package org.epics.archiverappliance.config;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 /**
  * Small unit test to make sure the regex that checks for valid pv names does something reasonable
@@ -11,43 +11,70 @@ import org.junit.Test;
  */
 public class PVNameRegexTest {
 
-	@Test
-	public void test() {
-		assertTrue("Checking for null pvName failed", !PVNames.isValidPVName(null));
-		assertTrue("Checking for empty pvName failed", !PVNames.isValidPVName(""));
-		String pvName = "";
-		pvName = "archappl:arch:sine"; assertTrue("Valid pvName is deemed invalid " + pvName, PVNames.isValidPVName(pvName));
-		pvName = "archappl:arch:sine1"; assertTrue("Valid pvName is deemed invalid " + pvName, PVNames.isValidPVName(pvName));
-		pvName = "archappl:arch:sine1.HIHI"; assertTrue("Valid pvName is deemed invalid " + pvName, PVNames.isValidPVName(pvName));
-		pvName = "archappl:arch:sine100"; assertTrue("Valid pvName is deemed invalid " + pvName, PVNames.isValidPVName(pvName));
-		pvName = "archappl_arch_sine"; assertTrue("Valid pvName is deemed invalid " + pvName, PVNames.isValidPVName(pvName));
-		pvName = "archappl-arch-sine"; assertTrue("Valid pvName is deemed invalid " + pvName, PVNames.isValidPVName(pvName));
-		pvName = "archappl+arch+sine"; assertTrue("Valid pvName is deemed invalid " + pvName, PVNames.isValidPVName(pvName));
-		pvName = "archappl[arch]sine"; assertTrue("Valid pvName is deemed invalid " + pvName, PVNames.isValidPVName(pvName));
-		pvName = "archappl<arch>sine"; assertTrue("Valid pvName is deemed invalid " + pvName, PVNames.isValidPVName(pvName));
-		pvName = "archappl:arch;sine"; assertTrue("Valid pvName is deemed invalid " + pvName, PVNames.isValidPVName(pvName));
-		pvName = "archappl:ar/h:sine"; assertTrue("Valid pvName is deemed invalid " + pvName, PVNames.isValidPVName(pvName));
-		// Add for FRIB
-		pvName = "P#6:000357"; assertTrue("Valid pvName is deemed invalid " + pvName, PVNames.isValidPVName(pvName));
-		// Add for BNL
-		pvName = "LN-AM{RadMon:1}Lvl:Raw-I"; assertTrue("Valid pvName is deemed invalid " + pvName, PVNames.isValidPVName(pvName));
-		
-		// Let's test some invalid ones
-		pvName = "\"archappl:arch:sine"; assertTrue("Invalid pvName is deemed valid " + pvName, !PVNames.isValidPVName(pvName));
-		pvName = "archappl:arch:sine\""; assertTrue("Invalid pvName is deemed valid " + pvName, !PVNames.isValidPVName(pvName));
-		pvName = "archappl:ar'h:sine"; assertTrue("Invalid pvName is deemed valid " + pvName, !PVNames.isValidPVName(pvName));
-		pvName = "archappl:ar!h:sine"; assertTrue("Invalid pvName is deemed valid " + pvName, !PVNames.isValidPVName(pvName));
-		pvName = "archappl:ar$h:sine"; assertTrue("Invalid pvName is deemed valid " + pvName, !PVNames.isValidPVName(pvName));
-		pvName = "archappl:ar%h:sine"; assertTrue("Invalid pvName is deemed valid " + pvName, !PVNames.isValidPVName(pvName));
-		pvName = "archappl:ar*h:sine"; assertTrue("Invalid pvName is deemed valid " + pvName, !PVNames.isValidPVName(pvName));
-		pvName = "archappl:ar\\h:sine"; assertTrue("Invalid pvName is deemed valid " + pvName, !PVNames.isValidPVName(pvName));
-		pvName = "archappl:ar|h:sine"; assertTrue("Invalid pvName is deemed valid " + pvName, !PVNames.isValidPVName(pvName));
-		pvName = "archappl:ar'h:sine"; assertTrue("Invalid pvName is deemed valid " + pvName, !PVNames.isValidPVName(pvName));
-		pvName = "archappl:ar?h:sine"; assertTrue("Invalid pvName is deemed valid " + pvName, !PVNames.isValidPVName(pvName));
-		pvName = "archappl:ar&h:sine"; assertTrue("Invalid pvName is deemed valid " + pvName, !PVNames.isValidPVName(pvName));
-		pvName = "archappl:ar@ch:sine"; assertTrue("Invalid pvName is deemed valid " + pvName, !PVNames.isValidPVName(pvName));
-		pvName = "archappl:(arch):sine"; assertTrue("Invalid pvName is deemed valid " + pvName, !PVNames.isValidPVName(pvName));
-		pvName = "archappl:arch:sine=1.0"; assertTrue("Invalid pvName is deemed valid " + pvName, !PVNames.isValidPVName(pvName));
-	}
+    @Test
+    public void test() {
+        assertTrue("Checking for null pvName failed", !PVNames.isValidPVName(null));
+        assertTrue("Checking for empty pvName failed", !PVNames.isValidPVName(""));
+        String pvName = "";
+        pvName = "archappl:arch:sine";
+        assertTrue("Valid pvName is deemed invalid " + pvName, PVNames.isValidPVName(pvName));
+        pvName = "archappl:arch:sine1";
+        assertTrue("Valid pvName is deemed invalid " + pvName, PVNames.isValidPVName(pvName));
+        pvName = "archappl:arch:sine1.HIHI";
+        assertTrue("Valid pvName is deemed invalid " + pvName, PVNames.isValidPVName(pvName));
+        pvName = "archappl:arch:sine100";
+        assertTrue("Valid pvName is deemed invalid " + pvName, PVNames.isValidPVName(pvName));
+        pvName = "archappl_arch_sine";
+        assertTrue("Valid pvName is deemed invalid " + pvName, PVNames.isValidPVName(pvName));
+        pvName = "archappl-arch-sine";
+        assertTrue("Valid pvName is deemed invalid " + pvName, PVNames.isValidPVName(pvName));
+        pvName = "archappl+arch+sine";
+        assertTrue("Valid pvName is deemed invalid " + pvName, PVNames.isValidPVName(pvName));
+        pvName = "archappl[arch]sine";
+        assertTrue("Valid pvName is deemed invalid " + pvName, PVNames.isValidPVName(pvName));
+        pvName = "archappl<arch>sine";
+        assertTrue("Valid pvName is deemed invalid " + pvName, PVNames.isValidPVName(pvName));
+        pvName = "archappl:arch;sine";
+        assertTrue("Valid pvName is deemed invalid " + pvName, PVNames.isValidPVName(pvName));
+        pvName = "archappl:ar/h:sine";
+        assertTrue("Valid pvName is deemed invalid " + pvName, PVNames.isValidPVName(pvName));
+        // Add for FRIB
+        pvName = "P#6:000357";
+        assertTrue("Valid pvName is deemed invalid " + pvName, PVNames.isValidPVName(pvName));
+        // Add for BNL
+        pvName = "LN-AM{RadMon:1}Lvl:Raw-I";
+        assertTrue("Valid pvName is deemed invalid " + pvName, PVNames.isValidPVName(pvName));
 
+        // Let's test some invalid ones
+        pvName = "\"archappl:arch:sine";
+        assertTrue("Invalid pvName is deemed valid " + pvName, !PVNames.isValidPVName(pvName));
+        pvName = "archappl:arch:sine\"";
+        assertTrue("Invalid pvName is deemed valid " + pvName, !PVNames.isValidPVName(pvName));
+        pvName = "archappl:ar'h:sine";
+        assertTrue("Invalid pvName is deemed valid " + pvName, !PVNames.isValidPVName(pvName));
+        pvName = "archappl:ar!h:sine";
+        assertTrue("Invalid pvName is deemed valid " + pvName, !PVNames.isValidPVName(pvName));
+        pvName = "archappl:ar$h:sine";
+        assertTrue("Invalid pvName is deemed valid " + pvName, !PVNames.isValidPVName(pvName));
+        pvName = "archappl:ar%h:sine";
+        assertTrue("Invalid pvName is deemed valid " + pvName, !PVNames.isValidPVName(pvName));
+        pvName = "archappl:ar*h:sine";
+        assertTrue("Invalid pvName is deemed valid " + pvName, !PVNames.isValidPVName(pvName));
+        pvName = "archappl:ar\\h:sine";
+        assertTrue("Invalid pvName is deemed valid " + pvName, !PVNames.isValidPVName(pvName));
+        pvName = "archappl:ar|h:sine";
+        assertTrue("Invalid pvName is deemed valid " + pvName, !PVNames.isValidPVName(pvName));
+        pvName = "archappl:ar'h:sine";
+        assertTrue("Invalid pvName is deemed valid " + pvName, !PVNames.isValidPVName(pvName));
+        pvName = "archappl:ar?h:sine";
+        assertTrue("Invalid pvName is deemed valid " + pvName, !PVNames.isValidPVName(pvName));
+        pvName = "archappl:ar&h:sine";
+        assertTrue("Invalid pvName is deemed valid " + pvName, !PVNames.isValidPVName(pvName));
+        pvName = "archappl:ar@ch:sine";
+        assertTrue("Invalid pvName is deemed valid " + pvName, !PVNames.isValidPVName(pvName));
+        pvName = "archappl:(arch):sine";
+        assertTrue("Invalid pvName is deemed valid " + pvName, !PVNames.isValidPVName(pvName));
+        pvName = "archappl:arch:sine=1.0";
+        assertTrue("Invalid pvName is deemed valid " + pvName, !PVNames.isValidPVName(pvName));
+    }
 }
