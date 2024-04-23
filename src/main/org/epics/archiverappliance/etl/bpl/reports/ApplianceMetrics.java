4a56ba77296c0eace08597dffad7eb80432bc959
Sky Brewer <sky.brewer@ess.eu>
2024-04-23 16:32:04 +0200

    Format etl metrics api

--- src/main/org/epics/archiverappliance/etl/bpl/reports/ApplianceMetrics.java
+++ src/main/org/epics/archiverappliance/etl/bpl/reports/ApplianceMetrics.java
@@ -7,21 +7,20 @@
  *******************************************************************************/
 package org.epics.archiverappliance.etl.bpl.reports;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.text.DecimalFormat;
-import java.util.HashMap;
-import java.util.List;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.epics.archiverappliance.common.BPLAction;
 import org.epics.archiverappliance.common.TimeUtils;
 import org.epics.archiverappliance.config.ConfigService;
 import org.epics.archiverappliance.etl.common.ETLMetricsForLifetime;
 import org.epics.archiverappliance.utils.ui.MimeTypeConstants;
 import org.json.simple.JSONValue;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.DecimalFormat;
+import java.util.HashMap;
+import java.util.List;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * Gets the ETL appliance metrics
@@ -30,32 +29,40 @@ import org.json.simple.JSONValue;
  */
 public class ApplianceMetrics implements BPLAction {
 
-	@Override
-	public void execute(HttpServletRequest req, HttpServletResponse resp, ConfigService configService) throws IOException {
-		resp.setContentType(MimeTypeConstants.APPLICATION_JSON);
-		try (PrintWriter out = resp.getWriter()) {
-			out.println(getETLMetrics(configService));
-		}
-	}
-	
-	private String getETLMetrics(ConfigService configService) {
-		HashMap<String, String> metrics = new HashMap<String, String>();
-		List<ETLMetricsForLifetime> metricsForLifetime = configService.getETLLookup().getApplianceMetrics();
-		if(metricsForLifetime == null || metricsForLifetime.size() < 1) {
-			metrics.put("Startup", "In Progress");
-		} else { 
-			double maxETLPercentage = 0.0; 
-			long currentEpochSeconds = TimeUtils.getCurrentEpochSeconds();
-			for(ETLMetricsForLifetime metricForLifetime : metricsForLifetime) {
-				double etlPercentage = (metricForLifetime.getTimeForOverallETLInMilliSeconds()/1000)*100/(currentEpochSeconds - metricForLifetime.getStartOfMetricsMeasurementInEpochSeconds());
-				maxETLPercentage = Math.max(etlPercentage, maxETLPercentage);
-				metrics.put("totalETLRuns("+metricForLifetime.getLifeTimeId()+")", Long.toString(metricForLifetime.getTotalETLRuns()));
-				metrics.put("timeForOverallETLInSeconds("+metricForLifetime.getLifeTimeId()+")", Long.toString(metricForLifetime.getTimeForOverallETLInMilliSeconds()/1000));
-			}
-			DecimalFormat twoSignificantDigits = new DecimalFormat("###,###,###,###,###,###.##");
-			metrics.put("maxETLPercentage", twoSignificantDigits.format(maxETLPercentage));
-		}
+    @Override
+    public void execute(HttpServletRequest req, HttpServletResponse resp, ConfigService configService)
+            throws IOException {
+        resp.setContentType(MimeTypeConstants.APPLICATION_JSON);
+        try (PrintWriter out = resp.getWriter()) {
+            out.println(getETLMetrics(configService));
+        }
+    }
 
-		return JSONValue.toJSONString(metrics);
-	}
+    private String getETLMetrics(ConfigService configService) {
+        HashMap<String, String> metrics = new HashMap<String, String>();
+        List<ETLMetricsForLifetime> metricsForLifetime =
+                configService.getETLLookup().getApplianceMetrics();
+        if (metricsForLifetime == null || metricsForLifetime.size() < 1) {
+            metrics.put("Startup", "In Progress");
+        } else {
+            double maxETLPercentage = 0.0;
+            long currentEpochSeconds = TimeUtils.getCurrentEpochSeconds();
+            for (ETLMetricsForLifetime metricForLifetime : metricsForLifetime) {
+                double etlPercentage = (metricForLifetime.getTimeForOverallETLInMilliSeconds() / 1000)
+                        * 100
+                        / (currentEpochSeconds - metricForLifetime.getStartOfMetricsMeasurementInEpochSeconds());
+                maxETLPercentage = Math.max(etlPercentage, maxETLPercentage);
+                metrics.put(
+                        "totalETLRuns(" + metricForLifetime.getLifeTimeId() + ")",
+                        Long.toString(metricForLifetime.getTotalETLRuns()));
+                metrics.put(
+                        "timeForOverallETLInSeconds(" + metricForLifetime.getLifeTimeId() + ")",
+                        Long.toString(metricForLifetime.getTimeForOverallETLInMilliSeconds() / 1000));
+            }
+            DecimalFormat twoSignificantDigits = new DecimalFormat("###,###,###,###,###,###.##");
+            metrics.put("maxETLPercentage", twoSignificantDigits.format(maxETLPercentage));
+        }
+
+        return JSONValue.toJSONString(metrics);
+    }
 }
--- src/main/org/epics/archiverappliance/etl/bpl/reports/InstanceReportDetails.java
+++ src/main/org/epics/archiverappliance/etl/bpl/reports/InstanceReportDetails.java
@@ -1,20 +1,20 @@
 package org.epics.archiverappliance.etl.bpl.reports;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.epics.archiverappliance.common.BPLAction;
 import org.epics.archiverappliance.config.ConfigService;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 public class InstanceReportDetails implements BPLAction {
 
-	@Override
-	public void execute(HttpServletRequest req, HttpServletResponse resp, ConfigService configService) throws IOException {
-		try (PrintWriter out = resp.getWriter()) {
-			out.println(ApplianceMetricsDetails.getETLMetricsDetails(configService));
-		}
-	}
+    @Override
+    public void execute(HttpServletRequest req, HttpServletResponse resp, ConfigService configService)
+            throws IOException {
+        try (PrintWriter out = resp.getWriter()) {
+            out.println(ApplianceMetricsDetails.getETLMetricsDetails(configService));
+        }
+    }
 }
