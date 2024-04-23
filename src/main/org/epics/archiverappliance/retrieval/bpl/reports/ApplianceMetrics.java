02b4a178e6bf95c327927ac002f33ad4ea8a8a81
Sky Brewer <sky.brewer@ess.eu>
2024-04-23 16:23:35 +0200

    Format Retrieval metrics api

--- src/main/org/epics/archiverappliance/retrieval/bpl/reports/ApplianceMetrics.java
+++ src/main/org/epics/archiverappliance/retrieval/bpl/reports/ApplianceMetrics.java
@@ -7,17 +7,16 @@
  *******************************************************************************/
 package org.epics.archiverappliance.retrieval.bpl.reports;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.HashMap;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.epics.archiverappliance.common.BPLAction;
 import org.epics.archiverappliance.config.ConfigService;
 import org.epics.archiverappliance.utils.ui.MimeTypeConstants;
 import org.json.simple.JSONValue;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * Summary metrics for retrieval for an alliance.
@@ -26,13 +25,13 @@ import org.json.simple.JSONValue;
  */
 public class ApplianceMetrics implements BPLAction {
 
-	@Override
-	public void execute(HttpServletRequest req, HttpServletResponse resp, ConfigService configService) throws IOException {
-		resp.setContentType(MimeTypeConstants.APPLICATION_JSON);
-		try (PrintWriter out = resp.getWriter()) {
-			HashMap<String, String> ret = new HashMap<String, String>();
-			out.println(JSONValue.toJSONString(ret));
-		}
-	}
-
+    @Override
+    public void execute(HttpServletRequest req, HttpServletResponse resp, ConfigService configService)
+            throws IOException {
+        resp.setContentType(MimeTypeConstants.APPLICATION_JSON);
+        try (PrintWriter out = resp.getWriter()) {
+            HashMap<String, String> ret = new HashMap<String, String>();
+            out.println(JSONValue.toJSONString(ret));
+        }
+    }
 }
--- src/main/org/epics/archiverappliance/retrieval/bpl/reports/ApplianceMetricsDetails.java
+++ src/main/org/epics/archiverappliance/retrieval/bpl/reports/ApplianceMetricsDetails.java
@@ -7,18 +7,17 @@
  *******************************************************************************/
 package org.epics.archiverappliance.retrieval.bpl.reports;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.HashMap;
-import java.util.LinkedList;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.epics.archiverappliance.common.BPLAction;
 import org.epics.archiverappliance.config.ConfigService;
 import org.epics.archiverappliance.utils.ui.MimeTypeConstants;
 import org.json.simple.JSONValue;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.LinkedList;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * Detail metrics for retrieval for an alliance.
@@ -27,13 +26,13 @@ import org.json.simple.JSONValue;
  */
 public class ApplianceMetricsDetails implements BPLAction {
 
-	@Override
-	public void execute(HttpServletRequest req, HttpServletResponse resp, ConfigService configService) throws IOException {
-		resp.setContentType(MimeTypeConstants.APPLICATION_JSON);
-		try (PrintWriter out = resp.getWriter()) {
-			LinkedList<HashMap<String, String>> ret = new LinkedList<HashMap<String, String>>();
-			out.println(JSONValue.toJSONString(ret));
-		}
-	}
-
+    @Override
+    public void execute(HttpServletRequest req, HttpServletResponse resp, ConfigService configService)
+            throws IOException {
+        resp.setContentType(MimeTypeConstants.APPLICATION_JSON);
+        try (PrintWriter out = resp.getWriter()) {
+            LinkedList<HashMap<String, String>> ret = new LinkedList<HashMap<String, String>>();
+            out.println(JSONValue.toJSONString(ret));
+        }
+    }
 }
