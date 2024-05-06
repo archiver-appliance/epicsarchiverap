c005bad59254721aad7d46e900cf013f67302746
Sky Brewer <sky.brewer@ess.eu>
2024-05-06 15:59:15 +0200

    Remove unused import

--- src/main/org/epics/archiverappliance/mgmt/bpl/DeletePV.java
+++ src/main/org/epics/archiverappliance/mgmt/bpl/DeletePV.java
@@ -1,6 +1,5 @@
 package org.epics.archiverappliance.mgmt.bpl;
 
-import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.epics.archiverappliance.common.BPLAction;
