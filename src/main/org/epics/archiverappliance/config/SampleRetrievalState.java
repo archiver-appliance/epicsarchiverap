a17a67726021824c84d2ca3da7e07bea5a13cb3a
Sky Brewer <sky.brewer@ess.eu>
2024-05-27 10:44:54 +0200

    Reformat SampleRetrievalState

--- src/main/org/epics/archiverappliance/config/SampleRetrievalState.java
+++ src/main/org/epics/archiverappliance/config/SampleRetrievalState.java
@@ -1,6 +1,8 @@
 package org.epics.archiverappliance.config;
 
+import edu.stanford.slac.archiverappliance.plain.PlainFileHandler;
 import edu.stanford.slac.archiverappliance.plain.PlainStoragePlugin;
+import edu.stanford.slac.archiverappliance.plain.pb.PBPlainFileHandler;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.epics.archiverappliance.StoragePlugin;
@@ -11,13 +13,14 @@ import org.epics.archiverappliance.retrieval.RetrievalState;
 import java.io.File;
 import java.io.IOException;
 import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 
 public class SampleRetrievalState extends RetrievalState {
-    private static Logger logger = LogManager.getLogger(SampleRetrievalState.class.getName());
+    private static final Logger logger = LogManager.getLogger(SampleRetrievalState.class.getName());
     ConfigServiceForTests configService;
 
     public SampleRetrievalState(ConfigServiceForTests parentConfigService) {
@@ -45,6 +48,7 @@ public class SampleRetrievalState extends RetrievalState {
     /**
      * The original unit tests were all constructed with YEAR partitions in mind.
      * So we change the sample config service to continue this for the unit test PV's.
+     *
      * @param pvName
      * @return
      */
@@ -58,7 +62,8 @@ public class SampleRetrievalState extends RetrievalState {
                         new File("../../../../../src/test/org/epics/archiverappliance/retrieval/channelarchiver");
                 assert (dataFile.exists());
                 String dataSrcURL = "rtree://localhost?serverURL="
-                        + URLEncoder.encode("file://" + dataFile.getAbsolutePath(), "UTF-8") + "&archiveKey=1";
+                        + URLEncoder.encode("file://" + dataFile.getAbsolutePath(), StandardCharsets.UTF_8)
+                        + "&archiveKey=1";
                 StoragePlugin caStoragePlugin = StoragePluginURLParser.parseStoragePlugin(dataSrcURL, configService);
                 datasources.add(new DataSourceforPV(pvName, caStoragePlugin, 1, null, null));
                 return datasources;
@@ -68,14 +73,16 @@ public class SampleRetrievalState extends RetrievalState {
             }
         }
 
+        PlainFileHandler plainFileHandler = new PBPlainFileHandler();
         PlainStoragePlugin mediumTermStore = (PlainStoragePlugin) StoragePluginURLParser.parseStoragePlugin(
-                "pb://localhost?name=MTS&rootFolder=" + configService.rootFolder
+                plainFileHandler.pluginIdentifier() + "://localhost?name=MTS&rootFolder=" + configService.rootFolder
                         + "&partitionGranularity=PARTITION_YEAR",
                 configService);
         datasources.add(new DataSourceforPV(pvName, mediumTermStore, 1, null, null));
 
         PlainStoragePlugin shortTermStore = (PlainStoragePlugin) StoragePluginURLParser.parseStoragePlugin(
-                "pb://localhost?name=STS&rootFolder=" + ConfigServiceForTests.DEFAULT_PB_SHORT_TERM_TEST_DATA_FOLDER
+                plainFileHandler.pluginIdentifier() + "://localhost?name=STS&rootFolder="
+                        + ConfigServiceForTests.DEFAULT_PB_SHORT_TERM_TEST_DATA_FOLDER
                         + "&partitionGranularity=PARTITION_YEAR",
                 configService);
         datasources.add(new DataSourceforPV(pvName, shortTermStore, 0, null, null));
