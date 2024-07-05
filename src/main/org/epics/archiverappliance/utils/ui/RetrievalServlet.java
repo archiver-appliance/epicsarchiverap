b9ba3971cba5962a756e8b98839add60c9764889
skybrewer <sky.brewer@ess.eu>
2024-07-05 15:17:12 +0200

    Format RetrievalServlet with spotless

--- src/main/org/epics/archiverappliance/utils/ui/RetrievalServlet.java
+++ src/main/org/epics/archiverappliance/utils/ui/RetrievalServlet.java
@@ -7,7 +7,6 @@
  *******************************************************************************/
 package org.epics.archiverappliance.utils.ui;
 
-
 import edu.stanford.slac.archiverappliance.plain.PlainStoragePlugin;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -17,13 +16,13 @@ import org.epics.archiverappliance.common.TimeUtils;
 import org.epics.archiverappliance.retrieval.postprocessors.DefaultRawPostProcessor;
 import org.epics.archiverappliance.retrieval.workers.CurrentThreadWorkerEventStream;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.time.Instant;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.time.Instant;
 
 /**
  * This is currently the code that remotes an event stream, for example from the engine etc.
@@ -32,50 +31,50 @@ import java.time.Instant;
  *
  */
 @SuppressWarnings("serial")
-public class RetrievalServlet  extends HttpServlet {
-	private static Logger logger = LogManager.getLogger(RetrievalServlet.class.getName());
-	String pbRootFolder = null;
+public class RetrievalServlet extends HttpServlet {
+    private static Logger logger = LogManager.getLogger(RetrievalServlet.class.getName());
+    String pbRootFolder = null;
 
-	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		PlainStoragePlugin storagePlugin = new PlainStoragePlugin();
-		storagePlugin.setRootFolder(pbRootFolder);
-		logger.info("Initialized the root folder to " + pbRootFolder);
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PlainStoragePlugin storagePlugin = new PlainStoragePlugin();
+        storagePlugin.setRootFolder(pbRootFolder);
+        logger.info("Initialized the root folder to " + pbRootFolder);
 
-		String PV = req.getParameter("pv");
-		String startTimeStr = req.getParameter("from"); 
-		String endTimeStr = req.getParameter("to");
-		
-		if(PV == null || startTimeStr == null || endTimeStr == null) {
-			resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
-			return;
-		}
+        String PV = req.getParameter("pv");
+        String startTimeStr = req.getParameter("from");
+        String endTimeStr = req.getParameter("to");
 
-		Instant start = TimeUtils.convertFromISO8601String(startTimeStr);
-		Instant end = TimeUtils.convertFromISO8601String(endTimeStr);
-		// resp.addHeader("Transfer-Encoding", "chunked");
-		
-		try(OutputStream os = resp.getOutputStream(); 
-				BasicContext context = new BasicContext(); 
-				EventStream st = new CurrentThreadWorkerEventStream(PV, storagePlugin.getDataForPV(context, PV, start, end, new DefaultRawPostProcessor()))) {
-			long s = System.currentTimeMillis();
-			int totalEvents = StreamPBIntoOutput.streamPBIntoOutputStream(st, os, start, end);
-			long e = System.currentTimeMillis();
-			logger.info("Found a total of " + totalEvents + " in " + (e-s) + "(ms)");
-		}
-	}
+        if (PV == null || startTimeStr == null || endTimeStr == null) {
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
 
-	@Override
-	public void init() throws ServletException {
-	}
-	
-	/**
-	 * Should only be used by the unit tests for setup...
-	 * @param rootFolder  &emsp; 
-	 * @return this  &emsp; 
-	 */  
-	public RetrievalServlet setpbRootFolder(String rootFolder) {
-		pbRootFolder = rootFolder;
-		return this;
-	}
+        Instant start = TimeUtils.convertFromISO8601String(startTimeStr);
+        Instant end = TimeUtils.convertFromISO8601String(endTimeStr);
+        // resp.addHeader("Transfer-Encoding", "chunked");
+
+        try (OutputStream os = resp.getOutputStream();
+                BasicContext context = new BasicContext();
+                EventStream st = new CurrentThreadWorkerEventStream(
+                        PV, storagePlugin.getDataForPV(context, PV, start, end, new DefaultRawPostProcessor()))) {
+            long s = System.currentTimeMillis();
+            int totalEvents = StreamPBIntoOutput.streamPBIntoOutputStream(st, os, start, end);
+            long e = System.currentTimeMillis();
+            logger.info("Found a total of " + totalEvents + " in " + (e - s) + "(ms)");
+        }
+    }
+
+    @Override
+    public void init() throws ServletException {}
+
+    /**
+     * Should only be used by the unit tests for setup...
+     * @param rootFolder  &emsp;
+     * @return this  &emsp;
+     */
+    public RetrievalServlet setpbRootFolder(String rootFolder) {
+        pbRootFolder = rootFolder;
+        return this;
+    }
 }
