6538631d0d4083ae298061cd848b5711917a6b4b
Murali Shankar <mshankar@slac.stanford.edu>
2024-08-30 07:00:58 -0700

    Make the server ioc drift a property

--- src/main/org/epics/archiverappliance/engine/model/ArchiveChannel.java
+++ src/main/org/epics/archiverappliance/engine/model/ArchiveChannel.java
@@ -64,7 +64,7 @@ public abstract class ArchiveChannel {
     private static final ThrottledLogger trouble_sample_log = new ThrottledLogger(LogLevel.info, 60);
 
     private static final Instant PAST_CUTOFF_TIMESTAMP = TimeUtils.convertFromISO8601String("1991-01-01T00:00:00.000Z");
-    private static final int SERVER_IOC_DRIFT_SECONDS = 30 * 60;
+    private final int SERVER_IOC_DRIFT_SECONDS;
     /**
      * Initialize the metafields for this channel. In addition to the metafields specified  here, we also generate PV's
      * for the runtime fields.
@@ -200,6 +200,7 @@ public abstract class ArchiveChannel {
             final boolean usePVAccess)
             throws Exception {
         this.name = name;
+        this.SERVER_IOC_DRIFT_SECONDS = Integer.parseInt(configservice.getInstallationProperties().getProperty("org.epics.archiverappliance.engine.epics.server_ioc_drift_seconds", "1800"));
         this.controlPVname = controlPVname;
         this.writer = writer;
         this.enablement = enablement;
