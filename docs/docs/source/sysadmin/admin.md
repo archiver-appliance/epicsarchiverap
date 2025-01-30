9f636c145c21c7eff87e3816f0c84c44acff3d41
Murali Shankar <mshankar@slac.stanford.edu>
2025-01-30 08:31:16 -0800

    Skip data store for retrieval based on named flag.
    Use a named flag based on the storage plugin's name.
    For example, if the name is LTS, the named flag
    SKIP_LTS_FOR_RETRIEVAL can used to temporarily turn off
    using the LTS for retrieval.
    
    Also add a BPL to get all named flags and their values

--- docs/docs/source/sysadmin/admin.md
+++ docs/docs/source/sysadmin/admin.md
@@ -168,3 +168,25 @@ Channel Access ( and PVAccess ). Here\'s an example of this process
 - Pausing and resuming the PV during packet capture will also enable
   the `cashark` plugin to track the life cycle of the Channel Access
   channel.
+
+## Temporarily suspend using a store
+
+If one of your data stores uses storage from elsewhere and you expect 
+an outage or disruption of connectivity, you can 
+temporarily suspend using the store for retrieval requests. 
+This is especially useful if you mount the remote location using NFS
+which can sometimes suspend threads forever during
+periods of network disruptions ( see the hard/soft/intr options for NFS clients).
+For example, if you have a data store named `LTS`, you can set the
+`SKIP_LTS_FOR_RETRIEVAL` named flag to `true` and this will stop adding the `LTS`
+to retrieval requests ( both `getData` and `getDataAtTime` ). Once the outage
+is over, you can turn `LTS` back on by setting the `SKIP_LTS_FOR_RETRIEVAL`
+named flag to `false`. Named flags are `false` by default; so restarting the
+appliances should also accomplish the same.
+
+
+
+Use a named flag based on the storage plugin's name.
+    For example, if the name is LTS, the named flag
+    SKIP_LTS_FOR_RETRIEVAL can used to temporarily turn off
+    using the LTS for retrieval.
---
+++ src/main/org/epics/archiverappliance/mgmt/bpl/NamedFlagsAll.java
@@ -0,0 +1,43 @@
+package org.epics.archiverappliance.mgmt.bpl;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.epics.archiverappliance.common.BPLAction;
+import org.epics.archiverappliance.config.ConfigService;
+import org.epics.archiverappliance.utils.ui.MimeTypeConstants;
+import org.json.simple.JSONObject;
+
+/**
+ * 
+ * @epics.BPLAction - Get all the named flags in the system and their values as a JSON dict
+ * @epics.BPLActionEnd
+ * 
+ * @author mshankar
+ *
+ */
+public class NamedFlagsAll implements BPLAction {
+	private static Logger logger = LogManager.getLogger(NamedFlagsAll.class.getName());
+	@Override
+	public void execute(HttpServletRequest req, HttpServletResponse resp, ConfigService configService) throws IOException {
+		HashMap<String, Boolean> ret = new HashMap<String, Boolean>();
+		for(String namedFlagName : configService.getNamedFlagNames()){
+			boolean namedValue = configService.getNamedFlag(namedFlagName);
+			ret.put(namedFlagName, namedValue);
+		}
+
+		resp.setContentType(MimeTypeConstants.APPLICATION_JSON);
+		try (PrintWriter out = resp.getWriter()) {
+			out.println(JSONObject.toJSONString(ret));
+		} catch(Exception ex) {
+			logger.error("Exception getting all named flags", ex);
+			resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+		}		
+	}
+}
