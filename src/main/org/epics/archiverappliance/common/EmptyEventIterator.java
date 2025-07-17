7481e0940b65f77c995aa086f8d0ebdf1cd1dfe0
Sky Brewer <sky.brewer@ess.eu>
2025-07-17 15:22:29 +0200

    format The event streams

--- src/main/org/epics/archiverappliance/common/EmptyEventIterator.java
+++ src/main/org/epics/archiverappliance/common/EmptyEventIterator.java
@@ -1,8 +1,8 @@
 package org.epics.archiverappliance.common;
 
-import java.util.Iterator;
-
 import org.epics.archiverappliance.Event;
+
+import java.util.Iterator;
 
 /**
  * An empty event interator.
@@ -10,17 +10,16 @@ import org.epics.archiverappliance.Event;
  *
  */
 public class EmptyEventIterator implements Iterator<Event> {
-	@Override
-	public boolean hasNext() {
-		return false;
-	}
+    @Override
+    public boolean hasNext() {
+        return false;
+    }
 
-	@Override
-	public Event next() {
-		return null;
-	}
+    @Override
+    public Event next() {
+        return null;
+    }
 
-	@Override
-	public void remove() {
-	}
+    @Override
+    public void remove() {}
 }
--- src/main/org/epics/archiverappliance/common/mergededup/TimeSpanLimitEventStream.java
+++ src/main/org/epics/archiverappliance/common/mergededup/TimeSpanLimitEventStream.java
@@ -19,73 +19,76 @@ import java.util.Iterator;
  *
  */
 public class TimeSpanLimitEventStream implements EventStream, RemotableOverRaw {
-	private static final Logger logger = LogManager.getLogger(TimeSpanLimitEventStream.class);
+    private static final Logger logger = LogManager.getLogger(TimeSpanLimitEventStream.class);
 
-	private final EventStream srcStream;
-	private final Instant startTime;
-	private final Instant endTime;
+    private final EventStream srcStream;
+    private final Instant startTime;
+    private final Instant endTime;
 
-	public TimeSpanLimitEventStream(EventStream srcStream, Instant startTime, Instant endTime) {
-		this.srcStream = srcStream;
-		this.startTime = startTime;
-		this.endTime = endTime;
-	}
+    public TimeSpanLimitEventStream(EventStream srcStream, Instant startTime, Instant endTime) {
+        this.srcStream = srcStream;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
 
-	private class MGIterator implements Iterator<Event> {
-		Iterator<Event> it = srcStream.iterator();
-		Event event = null;
-		
-		MGIterator() {
-			move();
-		}
-		
-		@Override
-		public boolean hasNext() {
-			return event != null;
-		}
+    private class MGIterator implements Iterator<Event> {
+        Iterator<Event> it = srcStream.iterator();
+        Event event = null;
 
-		@Override
-		public Event next() {
-			Event ret = null;
-			if(event != null) {
-				ret = event.makeClone();
-				move();
-			}
-			return ret;
-		}
-		
-		private void move() {
-			event = null;
-			if(it != null) {
-				while(it.hasNext()) {
-					Event nxtEvent = it.next();
-					Instant ts = nxtEvent.getEventTimeStamp();
-					if (ts.isBefore(startTime) || ts.isAfter(endTime)) {
-						logger.warn("Skipping event outside the time range " + TimeUtils.convertToHumanReadableString(ts));						
-					} else {
-						// logger.debug("Event inside the time range " + TimeUtils.convertToHumanReadableString(ts));
-						event = nxtEvent;
-						break;
-					}
-				}
-			}
-		}
-		
-	}
+        MGIterator() {
+            move();
+        }
 
-	@Override
-	public Iterator<Event> iterator() {
-		return new MGIterator();
-	}
+        @Override
+        public boolean hasNext() {
+            return event != null;
+        }
 
-	@Override
-	public void close() throws IOException {
-		try { this.srcStream.close(); } catch(IOException ex) { logger.error(ex); }
-	}
+        @Override
+        public Event next() {
+            Event ret = null;
+            if (event != null) {
+                ret = event.makeClone();
+                move();
+            }
+            return ret;
+        }
 
-	@Override
-	public RemotableEventStreamDesc getDescription() {
-		return (RemotableEventStreamDesc)srcStream.getDescription();
-	}
+        private void move() {
+            event = null;
+            if (it != null) {
+                while (it.hasNext()) {
+                    Event nxtEvent = it.next();
+                    Instant ts = nxtEvent.getEventTimeStamp();
+                    if (ts.isBefore(startTime) || ts.isAfter(endTime)) {
+                        logger.warn(
+                                "Skipping event outside the time range " + TimeUtils.convertToHumanReadableString(ts));
+                    } else {
+                        // logger.debug("Event inside the time range " + TimeUtils.convertToHumanReadableString(ts));
+                        event = nxtEvent;
+                        break;
+                    }
+                }
+            }
+        }
+    }
 
+    @Override
+    public Iterator<Event> iterator() {
+        return new MGIterator();
+    }
+
+    @Override
+    public void close() throws IOException {
+        try {
+            this.srcStream.close();
+        } catch (IOException ex) {
+            logger.error(ex);
+        }
+    }
+
+    @Override
+    public RemotableEventStreamDesc getDescription() {
+        return (RemotableEventStreamDesc) srcStream.getDescription();
+    }
 }
