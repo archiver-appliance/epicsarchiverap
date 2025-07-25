ae677609eeca828df4ac04fa501b43c92d403ff6
Sky Brewer <sky.brewer@ess.eu>
2025-07-25 09:16:09 +0200

    Change store method
    Adds a new method to the mgmt bpl
    The new method allows updating the store for a single PV, i.e.
    
     - to move from one MTS folder to another.
     - to convert from pb backend to parquet backend.
     - to convert from weekly partitions to monthly partitions
    
    Depending on the amount of data or type storage change this can take different ranges of time.

---
+++ src/main/org/epics/archiverappliance/etl/bpl/ChangeStore.java
@@ -0,0 +1,46 @@
+package org.epics.archiverappliance.etl.bpl;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.epics.archiverappliance.common.BPLAction;
+import org.epics.archiverappliance.config.ConfigService;
+import org.epics.archiverappliance.etl.ETLExecutor;
+import org.epics.archiverappliance.utils.ui.MimeTypeConstants;
+import org.json.simple.JSONValue;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class ChangeStore implements BPLAction {
+    private static final Logger logger = LogManager.getLogger(ChangeStore.class);
+
+    @Override
+    public void execute(HttpServletRequest req, HttpServletResponse resp, ConfigService configService)
+            throws IOException {
+        String pvName = req.getParameter("pv");
+        String storageName = req.getParameter("storage");
+        String newPlugin = req.getParameter("newPlugin");
+        if (StringUtils.isEmpty(pvName) || StringUtils.isEmpty(storageName) || StringUtils.isEmpty(newPlugin)) {
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+
+        try {
+            ETLExecutor.moveDataFromOneStorageToAnother(configService, pvName, storageName, newPlugin);
+            resp.setContentType(MimeTypeConstants.APPLICATION_JSON);
+            HashMap<String, Object> infoValues = new HashMap<String, Object>();
+            try (PrintWriter out = resp.getWriter()) {
+                infoValues.put("status", "ok");
+                infoValues.put("desc", "Successfully consolidated PV " + pvName + " into " + storageName);
+                out.println(JSONValue.toJSONString(infoValues));
+            }
+        } catch (IOException e) {
+            logger.error("Exception consolidating the partitions for pv: " + pvName + " into store: " + storageName, e);
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
+        }
+    }
+}
